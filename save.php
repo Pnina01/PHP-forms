700e5ca9d1a715c658c0a7c019ef5db52a27bb51
pnina <pnina@gmail.com>
2023-06-28 15:42:40 +0300

    actualizado todo el form  de php

--- save.php
+++ save.php
@@ -1,6 +1,13 @@
-<?
-print " your  data has been  saved";
+<?php
 
+$name = $_POST["name"];
+$price = $_POST["price"];
+$stock = $_POST["stock"];
 
+$line = "Name: $name, Price: $price, Stock: $stock" . PHP_EOL; // EOL = End Of Line = New Line
 
-?>
+// Saves the line to a text file named products.txt:
+file_put_contents("./products.txt", $line, FILE_APPEND);
+
+// Redirect: 
+header("location: thanks.php");
